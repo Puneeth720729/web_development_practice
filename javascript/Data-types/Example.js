67a885ad98bc8238c61a94e87661a12ec2995685
Puneeth_625 <160698716+Puneeth720729@users.noreply.github.com>
2025-06-05 22:49:36 +0530

    Create Example.js
    git commit -m "Added examples for Data-types in javascript."

---
+++ javascript/Data-types/Example.js
@@ -0,0 +1,34 @@
+// Number
+let num = 100;
+console.log("Number:", num, typeof num);
+
+// String
+let name = "Puneeth";
+console.log("String:", name, typeof name);
+
+// Boolean
+let isActive = true;
+console.log("Boolean:", isActive, typeof isActive);
+
+// Undefined
+let notAssigned;
+console.log("Undefined:", notAssigned, typeof notAssigned);
+
+// Null
+let emptyValue = null;
+console.log("Null:", emptyValue, typeof emptyValue);
+
+// Symbol
+let sym = Symbol("id");
+console.log("Symbol:", sym, typeof sym);
+
+// BigInt
+let bigNumber = 1234567890123456789012345678901234567890n;
+console.log("BigInt:", bigNumber, typeof bigNumber);
+
+// Object
+let user = {
+  name: "Ram",
+  age: 28,
+};
+console.log("Object:", user, typeof user);
